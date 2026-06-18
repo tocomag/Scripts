b82dcee30ca96bab439f68df1001f40d92e56be7
tocomag <ta1tu3to2106@gmail.com>
2026-06-18 12:14:53 +0900

    SphereGenerator.csを書きました

--- View/SphereGenerator.cs
+++ View/SphereGenerator.cs
@@ -2,15 +2,38 @@ using UnityEngine;
 
 public class SphereGenerator : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private GameObject prefab;
+    [SerializeField] private float radius;
+    [SerializeField] private float longtitudeInterval; // 緯度
+    [SerializeField] private float latitudeInterval; // 経度
+    [SerializeField] private int layer;
+    [SerializeField] private float reductionRatioPerLayer;
+
     void Start()
     {
-        
+        GenerateSphere();
     }
-
-    // Update is called once per frame
-    void Update()
+    public void GenerateSphere()
     {
-        
+        float local_radius = radius;
+        for (int l = 0; l < layer; l++)
+        {
+            for (float lo = longtitudeInterval; lo < 360f - longtitudeInterval; lo+=longtitudeInterval)
+            {
+                for (float la = latitudeInterval; la <= 180f - latitudeInterval; la+=latitudeInterval)
+                {
+                    float lo_rad = lo * Mathf.Deg2Rad;
+                    float la_rad = la * Mathf.Deg2Rad;
+                    Vector3 pos = new Vector3
+                    (
+                        local_radius * Mathf.Cos(lo_rad) * Mathf.Sin(la_rad) + transform.position.x,
+                        local_radius * Mathf.Cos(la_rad) + transform.position.y,
+                        local_radius * Mathf.Sin(lo_rad) * Mathf.Sin(la_rad) + transform.position.z
+                    );
+                    Instantiate(prefab, pos, Quaternion.identity, transform);
+                }
+            }
+            local_radius *= reductionRatioPerLayer; // 内側の層は半径が縮む
+        }   
     }
 }
