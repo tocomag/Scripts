f8cff408427bd9757d3d0790a725852de56c7dfd
tocomag <ta1tu3to2106@gmail.com>
2026-06-18 15:17:00 +0900

    SphereGeneratorに配列を追加
    この配列は「表示を配列で持つデータクラス」に渡し、そのクラスを用いて「表示処理を持つクラス」に渡すという流れを作るために配列を返すようにした。

--- View/SphereGenerator.cs
+++ View/SphereGenerator.cs
@@ -11,20 +11,21 @@ public class SphereGenerator : MonoBehaviour // 球状に盤面を生成するFa
 
     void Start()
     {
-        GenerateSphere();
+        GenerateSphere(); // いずれはGameManagerで実行する
     }
-    public void GenerateSphere()
+    public GameObject[,,] GenerateSphere()
     {
         int loCount = (int)(360f / longtitudeInterval); // 縦線の数
         int laCount = (int)(180f / latitudeInterval); // 横線の数
         float local_radius = radius;
+        GameObject[,,] objs = new GameObject[layer, loCount, laCount];
         for (int l = 0; l < layer; l++)
         {
             for (int lo = 0; lo < loCount; lo++)
             {
                 for (int la = 0; la < laCount; la++)
                 {
-                    if (la == 0) continue;
+                    if (la == 0) continue; // 一番上に置くと重なるので置かない
                     float lo_rad = lo * longtitudeInterval * Mathf.Deg2Rad;
                     float la_rad = la * latitudeInterval * Mathf.Deg2Rad;
                     Vector3 pos = new Vector3
@@ -33,10 +34,13 @@ public class SphereGenerator : MonoBehaviour // 球状に盤面を生成するFa
                         local_radius * Mathf.Cos(la_rad) + transform.position.y,
                         local_radius * Mathf.Sin(lo_rad) * Mathf.Sin(la_rad) + transform.position.z
                     );
-                    Instantiate(prefab, pos, Quaternion.identity, transform);
+                    GameObject obj = Instantiate(prefab, pos, Quaternion.identity, transform);
+                    // ここでobjにアタッチされたコンポーネントの値を変更する
+                    objs[l, lo, la] = obj;
                 }
             }
             local_radius *= reductionRatioPerLayer; // 内側の層は半径が縮む
-        }   
+        }
+        return objs; // 表示の処理を持つクラスに渡す
     }
 }
