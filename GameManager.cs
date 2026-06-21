fa7fdbad6c251dc873a907706a09587a18531246
tocomag <ta1tu3to2106@gmail.com>
2026-06-22 00:47:06 +0900

    ちょっと変えた

--- GameManager.cs
+++ GameManager.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 public class GameManager : MonoBehaviour
 {
-    private BoardSettings stgs;
+    [SerializeField] private BoardSettings stgs;
     private Board board;
     void Awake()
     {
